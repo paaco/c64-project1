84ef5b196658b857ccebc5b0c9b2a2417187a151
paaco <paaco@users.noreply.github.com>
2019-09-06 09:33:09 +0200

    Specify colors in filename with -bg and -mc

--- Sprite2asm.java
+++ Sprite2asm.java
@@ -6,15 +6,19 @@ import java.io.File;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 // Convert spritesheet to asm
-
-// TODO: use raster2mc when filename contains used colors (eg ".mc####")
 
 public class Sprite2asm {
 
     // text output: ACME style uses !byte, KickAssembler expects .byte, DreamAss uses .db
     private static final String PREFIX = "!byte";
+
+    // color specifiers
+    private static final Pattern BGPATTERN = Pattern.compile("-bg([0-9a-fA-F])"); // -bgX bg (forces hires)
+    private static final Pattern MCPATTERN = Pattern.compile("-mc([0-9a-fA-F])([0-9a-fA-F])"); // -mcXX mc1 mc2
 
     private Sprite2asm(String fname) {
         srcfilename = fname;
@@ -26,19 +30,37 @@ public class Sprite2asm {
         new Sprite2asm(args[0]).run();
     }
 
-    // bgIndex is '0', any other color is '1' (sprite color)
-    private byte[] raster2hires(Raster pixels, int xoff, int yoff, int bgIndex) {
+    private int pixel_width = 1; // default to hires
+    private int bgIndex = 0;     // black
+    private int mc1Index = 1;    // white
+    private int mc2Index = 2;    // red
+
+    // pixelWidth 1: bgIndex is '0', any other color is '1' (sprite color)
+    // pixelWidth 2: bgIndex is '00'(0), mc1Index is '01'(1), mc2Index is '11'(3), any other is '10'(2) (sprite color)
+    private byte[] extractSprite(Raster pixels, int xoff, int yoff) {
         byte[] buf = new byte[64];
         int bufoffset = 0;
         int bitcount = 0;
         byte b = 0;
         for (int y = 0; y < 21; y++) {
-            for (int x = 0; x < 24; x++) {
+            for (int x = 0; x < 24; x += pixel_width) {
                 int pixel = pixels.getSample(x + xoff, y + yoff, 0);
-                b <<= 1;
-                if (pixel != bgIndex) b |= 1;
+                b <<= pixel_width;
+
+                if (pixel_width == 1) {
+                    // hires
+                    if (pixel == bgIndex) b |= 0;
+                    else b |= 1;
+                } else {
+                    // mc
+                    if (pixel == bgIndex) b |= 0;
+                    else if (pixel == mc1Index) b |= 1;
+                    else if (pixel == mc2Index) b |= 3;
+                    else b |= 2;
+                }
+
                 // flush
-                bitcount++;
+                bitcount += pixel_width;
                 if (bitcount == 8) {
                     buf[bufoffset++] = b;
                     bitcount = 0;
@@ -49,34 +71,24 @@ public class Sprite2asm {
         return buf;
     }
 
-    // bgIndex is '00'(0), mc1Index is '01'(1), mc2Index is '11'(3), any other is '10'(2) (sprite color)
-    private byte[] raster2mc(Raster pixels, int xoff, int yoff, int bgIndex, int mc1Index, int mc2Index) {
-        byte[] buf = new byte[64];
-        int bufoffset = 0;
-        int bitcount = 0;
-        byte b = 0;
-        for (int y = 0; y < 21; y++) {
-            for (int x = 0; x < 24; x += 2) {
-                int pixel = pixels.getSample(x + xoff, y + yoff, 0);
-                b <<= 2;
-                if (pixel == bgIndex) b |= 0;
-                else if (pixel == mc1Index) b |= 1;
-                else if (pixel == mc2Index) b |= 3;
-                else b |= 2;
-                // flush
-                bitcount++;
-                if (bitcount == 4) {
-                    buf[bufoffset++] = b;
-                    bitcount = 0;
-                    b = 0;
-                }
-            }
+    // extract color format from string
+    private void updateColorMapping(String str) {
+        Matcher bg = BGPATTERN.matcher(str);
+        Matcher mc = MCPATTERN.matcher(str);
+        if (bg.find()) { // "-bgX" sets bg index and forces hires
+            bgIndex = Integer.parseInt(bg.group(1),16);
+            pixel_width = 1;
         }
-        return buf;
+        if (mc.find()) { // -mcXX sets mc1 and mc2 indices and forces mc
+            mc1Index = Integer.parseInt(mc.group(1),16);
+            mc2Index = Integer.parseInt(mc.group(2),16);
+            pixel_width = 2;
+        }
     }
 
     private void run() throws IOException {
         File f = new File(srcfilename);
+        updateColorMapping(srcfilename);
         BufferedImage image = ImageIO.read(f);
         int height = image.getHeight();
         int width = image.getWidth();
@@ -90,8 +102,7 @@ public class Sprite2asm {
             int nr = 0;
             for (int sy = 0; sy < height; sy += 21) {
                 for (int sx = 0; sx < width; sx += 24) {
-                    byte[] sprite = raster2hires(pixels, sx, sy, 0);
-                    //byte[] sprite = raster2mc(pixels, sx, sy, 0, 1, 2);
+                    byte[] sprite = extractSprite(pixels, sx, sy);
                     if (notEmpty(sprite)) {
                         StringBuilder sb = new StringBuilder();
                         sb.append(String.format("; %d (%d,%d)\n", nr, sx, sy));
